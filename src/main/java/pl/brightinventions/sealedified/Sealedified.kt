12d18f451cee096c1de62700947ed5d1922dbbf0
Andrzej Zabost <andrzej.zabost@brightinventions.pl>
2020-12-27 10:29:00 +0100

    Add T : Any constraint to Sealedified

--- src/main/java/pl/brightinventions/sealedified/Sealedified.kt
+++ src/main/java/pl/brightinventions/sealedified/Sealedified.kt
@@ -2,9 +2,9 @@ package pl.brightinventions.sealedified
 
 import kotlinx.serialization.json.JsonObject
 
-sealed class Sealedified<T> {
-    data class Known<T>(val value: T) : Sealedified<T>()
-    data class Unknown<T>(val jsonObject: JsonObject) : Sealedified<T>()
+sealed class Sealedified<T : Any> {
+    data class Known<T : Any>(val value: T) : Sealedified<T>()
+    data class Unknown<T : Any>(val jsonObject: JsonObject) : Sealedified<T>()
 
     fun knownOrNull() = (this as? Known<T>)?.value
 }
